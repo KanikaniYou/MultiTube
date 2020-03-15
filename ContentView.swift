da991642cb7881e295e58447a8c821b84aab7bd6
Харакири Молима <kanikani.you@gmail.com>
2020-03-15 16:38:31 +0900

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  MultiTube
+//
+//  Created by YUICHIRO MORI on 2020/03/15.
+//  Copyright © 2020 Mohrey Y. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+    GeometryReader { geometry in
+      VStack{
+            WebView(loadUrl: "https://www.twitter.com")
+              .frame(width: geometry.size.width, height: geometry.size.height * 7 / 10)
+            WebView(loadUrl: "https://www.youtube.com")
+        }
+     .edgesIgnoringSafeArea(.bottom)
+    }
+  }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WebView.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    var loadUrl:String
+    var allowsInlineMediaPlayback: Bool{true}
+
+    func makeUIView(context: Context) -> WKWebView {
+      let webConfiguration = WKWebViewConfiguration()
+      webConfiguration.allowsInlineMediaPlayback = true
+      return WKWebView(frame: .zero, configuration: webConfiguration)
+//      https://stackoverflow.com/questions/58902095/how-can-i-add-web-view-preferences-to-a-swift-ui-web-view
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        uiView.load(URLRequest(url: URL(string: loadUrl)!))
+    }
+}
